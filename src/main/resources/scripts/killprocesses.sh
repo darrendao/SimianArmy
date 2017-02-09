558543f82dbbae342093d40156818176dbcf4456
Darren Dao <darren_dao@intuit.com>
2017-02-09 09:17:49 -0800

    Use sudo for killing processes since we're using ec2-user and not root. Added more processes to kill

--- src/main/resources/scripts/killprocesses.sh
+++ src/main/resources/scripts/killprocesses.sh
@@ -5,10 +5,15 @@ cat << EOF > /tmp/kill_loop.sh
 #!/bin/bash
 while true;
 do
-    pkill -KILL -f java
-    pkill -KILL -f python
+    sudo pkill -KILL -f java
+    sudo pkill -KILL -f python
+    sudo pkill -KILL -f ruby
+    sudo pkill -KILL -f unicorn_rails
+    sudo pkill -KILL -f httpd
+    sudo pkill -KILL -f nginx
     sleep 1
 done
 EOF
 
 nohup /bin/bash /tmp/kill_loop.sh &
+sleep 5

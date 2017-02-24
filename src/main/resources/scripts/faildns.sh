2c525454304359a7f0b2f99ecd699e7699d76cae
Darren Dao <darren_dao@intuit.com>
2017-02-23 22:36:43 -0800

    Use sudo in other script as well

--- src/main/resources/scripts/faildns.sh
+++ src/main/resources/scripts/faildns.sh
@@ -2,5 +2,5 @@
 # Script for FailDns Chaos Monkey
 
 # Block all traffic on port 53
-iptables -A INPUT -p tcp -m tcp --dport 53 -j DROP
-iptables -A INPUT -p udp -m udp --dport 53 -j DROP
+sudo iptables -A INPUT -p tcp -m tcp --dport 53 -j DROP
+sudo iptables -A INPUT -p udp -m udp --dport 53 -j DROP
